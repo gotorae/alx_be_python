46e68bb3c31aa8df40fd57a0fc59f74df2a1b310
Effort Gotora <effortgotora123@gmail.com>
2025-09-19 14:22:50 +0200

    match casing the sympol

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,21 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+result = input("(+, -, *, \): ")
+
+match result:
+    case "+":
+        print(f"The result is {num1 + num2}")
+    case "*":
+        print(f"The result is {num1 * num2}")
+    case "-":
+        print(f"The result is {num1 - num2}")
+    case "/":
+        if num2 == 0:
+            print("cannot divide by zero")
+        else:
+            print(f"The result is {num1 - num2}")
+    case _:
+        print("invalid sympol entered")
+
+
+
