64e1d9a57d1a524c2e166c047e9103cefbf26f40
Effort Gotora <effortgotora123@gmail.com>
2025-09-19 15:01:09 +0200

    calculator match case

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,6 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
-result = input("(+, -, *, \): ")
+result = input("Choose the operation (+, -, *, /): ")
 
 match result:
     case "+":
