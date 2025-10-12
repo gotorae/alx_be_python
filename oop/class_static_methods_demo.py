054b2308540ede3f731e3a73bb8d201c980d078d
Effort Gotora <effortgotora123@gmail.com>
2025-10-12 20:21:01 +0200

    class'

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,16 @@
+# class_static_methods_demo.py
+
+class Calculator:
+    # Class attribute
+    calculation_type = "Arithmetic Operations"
+
+    # Static method: does not access the class itself
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    # Class method: accesses the class through cls
+    @classmethod
+    def multiply(cls, a, b):
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b
